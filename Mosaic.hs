0e67048d6daf31b3f92a117de3211f3f6f5c7bd8
Frederik Kreckler <frederik.kreckler@t-online.de>
2010-05-03 19:11:41 +0200

    Commenting milestone and renaming dummy
    Optimizations will follow

--- Mosaic.hs
+++ Mosaic.hs
@@ -19,6 +19,8 @@ main = do x <- readPPM "boat.ppm"
           --print $ w x
           return ()
 
+-- takes a list of strings (representing lines of a ppm file) 
+-- and turns them into a list of pixels
 str2pix :: [String] -> [Pixel]
 str2pix [] = []
 str2pix (r:g:b:xs) = do let np = Pixel{
@@ -27,19 +29,22 @@ str2pix (r:g:b:xs) = do let np = Pixel{
                                  pb = read b}
                         np:(str2pix xs)
 
+-- takes a filename and opens it as a ppm image
 readPPM :: String -> IO(Image)
 readPPM fn = do c <- readFile fn
                 let content = lines c
-                let dummy2 = Image {
+                let res = Image {
                              w = read (words (content !! 1) !! 0) ::Int,
                              h = read (words (content !! 1) !! 1) ::Int,
                              pxs = str2pix $ concat $ map words (drop 3 content)
                              }
-                return dummy2
+                return res
 
+-- takes a single pixel and returns its contents as a string
 stringify :: Pixel -> String
 stringify px = unwords (show(pr px):show(pg px):show(pb px):[])
 
+-- writes a ppm image under a given filename
 writePPM :: String -> Image -> IO()
 writePPM fn img = do writeFile fn $ printf "P3\n%d %d\n255\n" (w img) (h img)
                      appendFile fn (unwords $ map stringify $ pxs img)

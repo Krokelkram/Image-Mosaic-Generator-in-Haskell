a3f544fca5044b8963f3981cbf4328cb7dc6fd12
Frederik Kreckler <frederik.kreckler@t-online.de>
2010-05-15 18:53:08 +0200

    a given image can be split into columns of a given length

--- Mosaic.hs
+++ Mosaic.hs
@@ -28,13 +28,62 @@ data Image = Image {
 
 start ::[String] -> IO ()
 start ("analyse":arg:_) = do
-  dirContent <- getDirectoryContents $ arg
+  dirContent <- getDirectoryContents arg
   images     <- mapM readPPM (filter (isSuffixOf ".ppm") dirContent)
   writeStats images
 start ("generate":arg:_) = do
   putStrLn "Image will be generated here..."
+  originalImg <- readPPM arg
+  let horiCuts = map (*imageWidth originalImg) $ cutPos (imageHeight originalImg) 3
+  let vertiCuts = cutPos (imageWidth originalImg) 3
+  --let subimages = map ( `take` (imageData originalImg)) horiCuts
+  --print $ imageData originalImg
+  --print horiCuts
+  let scheiben =  cut horiCuts (imageData originalImg)
+  --print scheiben
+  --let bla = (cutVert vertiCuts 1)
+  --print $ concat $ map (reverse.cutVert vertiCuts 2) scheiben
+  --print $ cutVert vertiCuts 2 (scheiben !! 0)
+
+  let lines = linesplit (imageWidth originalImg) (imageData originalImg)
+  let splitters =  concat $ map (columnsplit vertiCuts) lines
+  --print splitters
+  let columns = map (extractColumn 3 splitters) [1..3]
+  print columns
+  
+  return ()
 start _ = putStrLn "Unknown parameter"
- 
+
+-- splits the pixellist into lines
+linesplit :: Int -> [Pixel] -> [[Pixel]]
+linesplit _ [] = []
+linesplit width pxs = take width pxs : linesplit width (drop width pxs)
+
+-- splits an image line into parts of given length
+columnsplit :: [Int] -> [Pixel] -> [[Pixel]]
+columnsplit _ [] = []
+columnsplit cuts pxs = take (head cuts) pxs : columnsplit (tail cuts) (drop (head cuts) pxs)
+
+-- extracts a single column with given index from the image
+extractColumn :: Int -> [[Pixel]] -> Int -> [[Pixel]]
+extractColumn _ [] _ = []
+extractColumn columns pxs index = pxs !! (index-1) : extractColumn columns (drop columns pxs) index
+
+-- extracts all subimages of a column using the supplied lengths
+--extractSubImage :: [Int] -> 
+
+
+cut :: [Int] -> [Pixel] -> [[Pixel]]
+cut [] _ = [] 
+cut cutPos pxs = (take (head cutPos) pxs) : (cut (tail cutPos) (drop (head cutPos) pxs) )
+
+-- takes a length (either width or height) and the number of parts we want to cut it into
+-- returns the length of each parts (cutPos 10 3 would return [3,3,4])
+cutPos :: Int -> Int -> [Int]
+cutPos _ 0 = []
+cutPos height parts = rowsToTake : cutPos (height - rowsToTake) (parts - 1)
+    where rowsToTake = (height `div` parts)
+
 
 main :: IO ()
 main = start =<< getArgs

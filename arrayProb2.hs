1047056bc70762a627623932fd63e284850baf9e
Michael Lesniak <michael.lesniak@gmail.com>
2010-05-22 08:35:22 +0200

    Fixed version for Arrays and Images.
    - Granted, the additional s Parameter needed for the ST monad is mean. But
      take a look at the Image class: in your version there was no possibility
      where the s of ST s could have come from; in mine it is actually a class
      parameter and therefore well defined.
    - General advise: some monads are plain mean and difficutl to understand,
      especially when you are dealing with typeclasses for the first time. Your
      work is really, really good! :)
    - Have a nice weekend.

--- arrayProb2.hs
+++ arrayProb2.hs
@@ -1,34 +1,54 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 import Control.Monad.ST.Strict
 import Data.Array.ST
 
-class Image a where
-    get ::  (Int, Int) -> a -> ST s Int
-    set :: (Int, Int) -> a -> Int -> ST s a
-    width :: a -> Int
-    height :: a -> Int
+class Image a s where
+    -- Makes more sense and eases programming later on when the first
+    -- parameter is the actual data instance.
+    get     :: a -> (Int, Int) -> ST s Int
+    set     :: a -> (Int, Int) -> Int -> ST s ()
+    width   :: a -> Int
+    height  :: a -> Int
+
 
 data PPMImage s = PPMImage {
-    ppmArray :: STArray s (Int,Int) Int
-    }
+    ppmData :: STArray s (Int,Int) Int
+}
 
--- was muss ich hier anpassen, damit das 's' mitgeht?
--- und falls das so stimmt: der Fehler lautet
--- Could not deduce (MArray (STArray s) Int (ST s1)) from context ()
-instance Image (PPMImage s) where
-    get (x,y) img = do
-      value <- readArray (ppmArray img) (x,y) :: ST s Int
-      return value
-    set (x,y) img px = do
-      value <- writeArray (ppmArray img) (x,y) px
-      return value
-
-useArray :: ST s Int
-useArray = do
-  array <- newArray ((1,1),(10,10)) 42 :: ST s (STArray s (Int,Int) Int)
-  let img = PPMImage array
-  a <- readArray (ppmArray img) (2,2)
-  return a
 
 main :: IO()
-main = print $ runST useArray
+main = print $ runST $ do
+    array <- newArray ((1,1),(10,10)) 42 :: ST s (STArray s (Int,Int) Int)
+    let image = PPMImage array
+
+    a1 <- readArray (ppmData image) (2,2)
+    -- PPMImage is an instance of Image, hence we can also do:
+    a2 <- get image (2,2)
+
+    return $ a1 == a2
+
+
+instance Image (PPMImage s) s where
+    get img (x,y) = do
+      -- A construct 
+      --
+      --   value <- monadicOp
+      --   return value
+      --
+      -- is equivalent to
+      --
+      --   monadicOp
+      --
+      -- Just look at the types :D
+      readArray (ppmData img) (x,y) 
+    
+    set img (x,y) px = do
+        -- writeArray changes the *state* of the array, so it does not return
+        -- a value; so, as long as you are inside the particular ST instance,
+        -- you actually have mutable state!
+        writeArray (ppmData img) (x,y) px
+
+    -- Need to be defined, too.
+    width _  = 42
+    height _ = 42
 

4dc1ee74d15b0d0469ecdf0309350f40e9bcfe9d
Frederik Kreckler <frederik.kreckler@t-online.de>
2010-05-23 15:30:06 +0200

    Image splitting works (test with test6x6.ppm, will split into 4 parts)

--- Mosaic.hs
+++ Mosaic.hs
@@ -27,6 +27,8 @@ data SubImage s = SubImage {
      parent :: PPMImage s
     ,offsetX :: Int
     ,offsetY :: Int
+    ,subWidth :: Int
+    ,subHeight :: Int
     }
 
 instance Image (PPMImage s) s where
@@ -39,17 +41,15 @@ instance Image (PPMImage s) s where
     height _ = 42
 
 instance Image (SubImage s) s where
-    get (SubImage parent x' y') (x,y) = 
+    get (SubImage parent x' y' _ _) (x,y) = 
         let (rx, ry) = (x'+x , y'+y)
         in get parent (rx,ry)
-    set (SubImage parent x' y') (x,y) =
+    set (SubImage parent x' y' _ _) (x,y) =
         let (rx, ry) = (x'+x , y'+y)
         in set parent (rx,ry)
     width _  = 42
     height _ = 42
       
-
-
 data Pixel = Pixel {
       pR :: Int
     , pG :: Int
@@ -60,28 +60,13 @@ data Pixel = Pixel {
 instance Show Pixel where
     show (Pixel r g b) = printf "%d %d %d" r g b
 
-
-data BadImage = BadImage {
-      imageWidth  :: Int
-    , imageHeight :: Int
-    , imageData   :: [Pixel]
-    , imageName   :: String
-    } deriving (Show, Eq)
-
+-- contains information about an image that is useful for mosaic tile matching (only medianColor so far)
 data Fingerprint = Fingerprint {
       fpFilename :: String
     , fpMedian :: Pixel
     } deriving (Show)
 
-
-myFirstArray:: ST s (Int,Int)
-myFirstArray = do
-  arr <- newListArray ((1,1), (2,2)) [1,2,3,4] :: ST s (STArray s (Int,Int) Int)
-  writeArray arr (1,1) 123
-  a <- readArray arr (1,2)
-  b <- readArray arr (1,1)
-  return (a,b)
-
+-- takes a filename and returns its statistics as a string
 analyseImage :: String -> IO String
 analyseImage filename = do
   rawImage <- BS.readFile filename
@@ -90,10 +75,21 @@ analyseImage filename = do
    statStr <- getStatString image
    return statStr
 
-
+-- takes a ppmImage that is split horizontally and vertically
+tileImage :: (PPMImage s) -> Int -> Int -> ST s [SubImage s]
+tileImage img partsX partsY = do
+  let array = ppmArray img
+  let w = ppmWidth img
+  let h = ppmHeight img
+  let ws = w `div` partsX
+  let hs = h `div` partsY
+  let xs = [x | x <- [0, ws..w-1]]
+  let ys = [y | y <- [0, hs..h-1]]
+  let cs = [(x,y) | x <- xs, y <- ys]
+  x <- mapM (\(x,y) -> return $ SubImage img x y ws hs) cs
+  return x
+  
 start ::[String] -> IO ()
-start ("foo":_) = do
-  print $ runST myFirstArray
 start ("analyse":arg:_) = do
   dirContent <- getDirectoryContents arg
   let filesWithPPMSuffix = (filter (isSuffixOf ".ppm") dirContent)
@@ -105,15 +101,18 @@ start ("generate":arg:_) = do
   putStrLn "Image will be generated here..."
   db <- readFile "DB.txt"
   let fingerprints = map dbLine2Fingerprint (lines db)
-  print fingerprints
+  --print fingerprints
   rawImage <- BS.readFile arg
   let originalImg = runST $ do 
-                 readPPM (rawImage, arg)
-                 return "To be implemented"
-
+                 image <- readPPM (rawImage, arg)
+                 tiles <- tileImage image 2 2
+                 subMedians <- mapM medianColorSub tiles
+                 return $ subMedians
+  print originalImg
   return ()
 start _ = putStrLn "Unknown parameter"
 
+-- takes a line from database and returns its information as an image fingerprint
 dbLine2Fingerprint :: String -> Fingerprint
 dbLine2Fingerprint ln = Fingerprint fn (Pixel (read r) (read g) (read b))
     where (fn:r:g:b:_) = words ln
@@ -122,6 +121,7 @@ dbLine2Fingerprint ln = Fingerprint fn (Pixel (read r) (read g) (read b))
 main :: IO ()
 main = start =<< getArgs
 
+-- takes a ppmImage and returns its statistics as a string
 getStatString :: (PPMImage s) -> ST s String
 getStatString img = do
   medCol <- medianColor img
@@ -161,3 +161,13 @@ medianColor image = do
     let blue =  sum (map pB listOfPixel) `div` ((ppmWidth image) * (ppmHeight image))
     return $ Pixel red green blue
 
+-- same as medianColor, but for SubImages. 
+-- Might be possible to merge these two later on
+medianColorSub :: (SubImage s) -> ST s Pixel
+medianColorSub image = do
+  let coords = [(x,y) | y <- [1..subHeight image], x <- [1..subWidth image]]
+  listOfPixel <- mapM (get image) coords
+  let red =  sum (map pR listOfPixel) `div` ((subWidth image) * (subHeight image))
+  let green =  sum (map pG listOfPixel) `div` ((subWidth image) * (subHeight image))
+  let blue =  sum (map pB listOfPixel) `div` ((subWidth image) * (subHeight image))
+  return $ Pixel red green blue

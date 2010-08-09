072a1b86ab4ccda8324a9f4ee81be02961621559
Frederik Kreckler <frederik.kreckler@t-online.de>
2010-08-09 17:40:21 +0200

    Friendly code

--- Mosaic.hs
+++ Mosaic.hs
@@ -9,7 +9,6 @@ import Data.Char
 import Data.Bits
 import Control.Parallel
 import Control.Monad
-import Debug.Trace
 import System.Console.CmdArgs
 import Graphics.GD.ByteString
 import Data.Time
@@ -20,28 +19,35 @@ main :: IO ()
 main = do
   modus <- cmdArgs "MosaicMaker v0.9" modes
   case modus of
-    (Analyse source db) -> trace (printf "Analyse path: %s" source) $ analyse source db
-    (Generate f o db' hTiles vTiles) -> trace (printf "Generate mosaic of: %s" f) $ generate f o db' hTiles vTiles
+    (Analyse source db) -> do
+               putStrLn $ printf "Analyse path: %s" source
+               (t,_) <- time $ analyse source db
+               putStrLn $ printf "Done in %fs" t
+    (Generate f o db' hTiles vTiles) -> do
+               putStrLn $ printf "Generate mosaic of: %s" f
+               (t, _) <- time $ generate f o db' hTiles vTiles
+               putStrLn $ printf "Done in %fs" t
     
 
 -- takes a filename and appends its statistics to a text file
 analyseImage :: String -> String ->IO ()
-analyseImage dbname filename = do
-  putStrLn $ printf "Starte Analyse von: %s" filename
-  avg <- saveResizedCopy filename
-  appendFile dbname $ printf "%s %s\n" filename (show avg)
-  putStrLn $ printf "%s wurde analysiert" filename
+analyseImage dbFolder filename = do
+  putStrLn $ printf "Analysing %s" filename
+  avg <- saveResizedCopy filename dbFolder
+  appendFile (dbFolder ++ "/" ++ "DB.txt") $ printf "%s %s\n" filename (show avg)
+  putStrLn "OK"
   
-saveResizedCopy :: String -> IO Pixel
-saveResizedCopy fn = do
-  let jpgname = fn
-  avg <- withImage (loadJpegFile jpgname) $ do (\x -> do
-                                                  resized <- resizeImage 80 60 x
-                                                  avg <- getJPGavg resized
-                                                  print avg
-                                                  saveJpegFile 95 (printf "%s_small.JPG" (takeWhile (/= '.') fn)) resized
-                                                  return avg)
-  return avg
+saveResizedCopy :: String -> String -> IO Pixel
+saveResizedCopy fn folder = 
+  withImage (loadJpegFile fn) (\x -> do
+                                 resized <- resizeImage 80 60 x
+                                 avg <- getJPGavg resized
+                                 print avg
+                                 saveJpegFile 95 (printf "%s/%s_small.JPG" folder (filenameOnly fn)) resized
+                                 return avg)
+      where filenameOnly path = removeExtension $ removeFolders path
+            removeFolders path = reverse $ takeWhile (/='/') (reverse path)
+            removeExtension = takeWhile (/= '.')
                                           
 getJPGavg :: Graphics.GD.ByteString.Image -> IO Pixel
 getJPGavg image = do
@@ -50,11 +56,11 @@ getJPGavg image = do
   pxs <- mapM (\x -> do 
                  colorC <- getPixel x image
                  let color = fromIntegral colorC :: Int
-                 return $ Pixel ((shiftR color 16) .&. 255) ((shiftR color 8) .&. 255) ( color .&. 255)) coords
-  let !red = sum (map pR pxs) `div` (length coords)
-  let !green = sum (map pG pxs) `div` (length coords)
-  let !blue = sum (map pB pxs) `div` (length coords)
-  let avgPx = Pixel red green blue
+                 return $ Pixel (shiftR color 16 .&. 255) (shiftR color 8 .&. 255) ( color .&. 255)) coords
+  let !red   = sum (map pR pxs) `div` length coords
+      !green = sum (map pG pxs) `div` length coords
+      !blue  = sum (map pB pxs) `div` length coords
+      avgPx  = Pixel red green blue
   return avgPx
    
 
@@ -65,10 +71,12 @@ analyse path dbFolder = do
   dirContent <- getDirectoryContents path
   let contentWithFullPath = map (\x -> path ++ "/" ++ x) dirContent
   print contentWithFullPath
-  writeFile "DB.txt" ""
-  let filesWithPPMSuffix = (filter ((isSuffixOf ".JPG")) contentWithFullPath)
-  mapM (analyseImage $ dbFolder ++ "/" ++ "DB.txt") filesWithPPMSuffix
-  print "Fertig"
+  createDirectoryIfMissing False dbFolder
+  writeFile (dbFolder ++ "/DB.txt") ""
+  let filesWithPPMSuffix = (filter isJPG contentWithFullPath)
+  mapM_ (analyseImage dbFolder) filesWithPPMSuffix
+  putStrLn "Done!"
+      where isJPG fn = (".JPG" `isSuffixOf` fn) || ".jpg" `isSuffixOf` fn
 
 
 -- Time a pure function. A should reduce to a single value, instead seq
@@ -77,22 +85,20 @@ pureTime ::(Show a) => [a] -> IO (Double, [a])
 pureTime action = do
     d1 <- getCurrentTime
     let a = action
-    d2 <- (length a) `seq` getCurrentTime
+    d2 <- length a `seq` getCurrentTime
     return (read . init $ show (diffUTCTime d2 d1), a)
 
 
 generate :: String -> String -> String -> Int -> Int ->IO ()
 generate fn outFn dbFolder hTiles vTiles = do
-  putStrLn "Bild wird generiert..."
   db <- readFile (dbFolder ++ "/" ++ "DB.txt")
   let fingerprints = map dbLine2Fingerprint (lines db)
   
   img <- loadJpegFile fn
   (w,h) <- imageSize img
-  let tileW = w `div` hTiles
-  let tileH = h `div` vTiles
-  let points = [(x,y) | y <- [0,tileH..h-tileH] , x <- [0,tileW..w-tileH] ]
-  putStrLn $ printf "Punkte: %d" (length points)
+  let tileW  = w `div` hTiles
+      tileH  = h `div` vTiles
+      points = [(x,y) | y <- [0,tileH..h-tileH] , x <- [0,tileW..w-tileH] ]
   tiles <- mapM (\p -> do
                    new <- newImage (tileW,tileH)
                    copyRegion p (tileW,tileH) img (0,0) new
@@ -100,24 +106,20 @@ generate fn outFn dbFolder hTiles vTiles = do
   avgs <- mapM getJPGavg tiles
 
   let bestImageNames = map (findMatch fingerprints) avgs
-  print bestImageNames
-  putStrLn $ printf "Beste Bilder: %d" (length bestImageNames)
 
   resImage <- newImage (80 * hTiles, 60 * vTiles)
   let offsets   = [(x,y) | y <- [0,60..(60*vTiles)-1], x <- [0,80..(80*hTiles)-1]]
       imgAndOff = zip bestImageNames offsets
-  --print imgAndOff
-  putStrLn $ printf "Offsets: %d" (length offsets)
-  putStrLn $ printf "ImgAndOffsets: %d" (length imgAndOff)
   mapM_ (insertImage resImage) imgAndOff
   saveJpegFile 95 outFn resImage
-  print "ENDE"
+  
 
 insertImage :: Graphics.GD.ByteString.Image -> (String, (Int,Int)) -> IO ()
 insertImage resImage (fn, offset) = do
-  let jpgname = trace (printf "Inserts: %s an %s" fn (show offset)) $ printf "%s_small.JPG" (takeWhile (/= '.') fn)
-  withImage (loadJpegFile jpgname) $ do (\x -> do
-                                           copyRegion (0,0) (80,60) x offset resImage)
+  isVerbose <- isLoud
+  when isVerbose $ putStrLn $ printf "Inserts: %s an %s" fn (show offset)
+  let jpgname = printf "%s_small.JPG" (takeWhile (/= '.') fn)
+  withImage (loadJpegFile jpgname) (\x -> copyRegion (0,0) (80,60) x offset resImage)
 
 -- Time an IO action
 time :: IO a -> IO (Double, a)
@@ -131,12 +133,11 @@ time action = do
 findMatch :: [Fingerprint] -> Pixel -> String
 findMatch fps pix = snd $ minDiff (map (colorDiff pix) fps)
     where minDiff [x] = x
-          minDiff (x1:x2:xs) = if (fst x1) < (fst x2)
+          minDiff (x1:x2:xs) = if fst x1 < fst x2
                                then minDiff $ x1:xs
                                else minDiff $ x2:xs
           minDiff [] = error "Invalid fingerprint"
           
-
 -- returns a tuple of color difference and filename
 colorDiff :: Pixel -> Fingerprint -> (Int, String)
 colorDiff p1 (Fingerprint fn p2) = (abs (abs(pR p1 - pR p2)+abs(pG p1 - pG p2)+abs(pB p1 - pB p2)), fn)
